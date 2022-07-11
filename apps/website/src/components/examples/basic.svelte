944314d4cce7fbf28a25716ac1be1c4499b7484e
kfras <kyle.fraser@fathomhq.com>
2022-07-11 22:59:55 +1000

    feat(core) Updated spline function

--- apps/website/src/components/examples/basic.svelte
+++ apps/website/src/components/examples/basic.svelte
@@ -1,6 +1,7 @@
 <Example>
     <div slot="sidebar">
         <input type="range" min="0" max="1" step="0.001" bind:value={time}>
+        <!-- <input type="range" min="0" max="1" step="0.001" bind:value={tension}> -->
     </div>
 </Example>
 
@@ -15,36 +16,62 @@ import {
     getContext,
     circle,
     spline,
-continuous,
+    continuous,
+    scene,
+    renderer,
+    TAU,
+    line,
+    drawLinePoints,
+    Point,
 } from '@ripl/core';
 
-let time = 0;
+let time = 1;
+let tension = 0.5;
 let render = (time: number) => {};
 
 $: {
     render(time);
 }
 
-onMount(() => {
+
+function mainDemo() {
     const {
         canvas,
         context,
         clear,
     } = getContext('.example__canvas');
 
-    const xScale = continuous([1, 5], [0, canvas.width]);
-    const yScale = continuous([0, 2], [canvas.height, 0]);
+    // RAND POINTS
+    const length = 10;
+    const xScale = continuous([0, length - 1], [10, canvas.width - 10]);
+    const yScale = continuous([-100, 100], [canvas.height - 10, 10]);
+    const rand = () => Math.random() * (Math.random() < 0.5 ? -1 : 1);
+    
+    const getPoints = () => Array.from({ length }, (_, i) => [
+        xScale(i),
+        yScale(rand() * 50)
+    ] as Point);
 
+    const points = getPoints();
+
+    // SINE DOMAIN 5 POINTS
+    // const xScale = continuous([1, 5], [0, canvas.width]);
+    // const yScale = continuous([0, 2], [canvas.height, 0]);
+
+    // const points = [
+    //         [xScale(1), yScale(1)],
+    //         [xScale(2), yScale(2)],
+    //         [xScale(3), yScale(1)],
+    //         [xScale(4), yScale(0)],
+    //         [xScale(5), yScale(1)],
+    //     ] as Point[];
+
+    
     const spl = spline({
         strokeStyle: '#000000',
         lineWidth: 4,
-        points: [
-            [xScale(1), yScale(1)],
-            [xScale(2), yScale(2)],
-            [xScale(3), yScale(1)],
-            [xScale(4), yScale(0)],
-            [xScale(5), yScale(1)],
-        ]
+        points: () => drawLinePoints(points)(time),
+        tension,
     });
 
     const crc = circle({
@@ -59,5 +86,9 @@ onMount(() => {
         clear();
         spl.render(context, time);
     }
+}
+
+onMount(() => {
+    mainDemo();
 });
 </script>
--- packages/core/src/elements/spline.ts
+++ packages/core/src/elements/spline.ts
@@ -16,7 +16,9 @@ import {
     getHypLength,
 } from '../math/trigonometry';
 
-export type Spline = Line;
+export interface Spline extends Line {
+    tension?: number;
+}
 
 function getControlPoint([x0, y0]: Point, [x1, y1]: Point, [x2, y2]: Point, tension: number = 0.5) {
     const d1 = getHypLength(x1 - x0, y1 - y0);
@@ -44,46 +46,82 @@ export const spline = shape<Spline>({
     onRender({ path, context, state }) {
         const {
             points,
+            tension,
         } = state;
 
-        // const pnts = [points[0]].concat(points, [points[points.length - 1]]);
+        const pnts = [points[0]].concat(points, [points[points.length - 1]]);
 
         // console.log(pnts);
 
-        // const controlPoints = Array.from({ length: pnts.length - 2 }, (_, index) => {
-        //     const [x0, y0] = pnts[index];
-        //     const [x1, y1] = pnts[index + 1];
-        //     const [x2, y2] = pnts[index + 2];
+        const controlPoints = Array.from({ length: pnts.length - 2 }, (_, index) => {
+            const [x0, y0] = pnts[index];
+            const [x1, y1] = pnts[index + 1];
+            const [x2, y2] = pnts[index + 2];
 
-        //     return getControlPoint([x0, y0], [x1, y1], [x2, y2]);
-        // });
+            return getControlPoint([x0, y0], [x1, y1], [x2, y2], tension);
+        });
 
         // console.log(controlPoints);
 
-        // path.moveTo(pnts[0][0], pnts[0][1]);
+        // const controlPointsLastIndex = controlPoints.length - 1;
+        const newCtrlPoints: number[][] = [];
+        for (let i = 0; i <= (controlPoints.length - 2); i++) {
+            // console.log('hello');
+            newCtrlPoints[i] = [
+                controlPoints[i][2],
+                controlPoints[i][3],
+                controlPoints[i + 1][0],
+                controlPoints[i + 1][1],
+            ];
 
-        // for (let index = 1; index <= points.length - 1; index++) {
-        //     const [x0, y0] = pnts[index - 1];
-        //     const [x1, y1] = pnts[index];
-        //     const [x2, y2] = pnts[index + 1];
+            if (i === 0) {
+                newCtrlPoints[i] = [
+                    controlPoints[i][0],
+                    controlPoints[i][1],
+                    controlPoints[i + 1][0],
+                    controlPoints[i + 1][1],
+                ];
+            }
 
-        //     const [cpx1, cpy1, cpx2, cpy2] = getControlPoint([x0, y0], [x1, y1], [x2, y2]);
+            if (i === controlPoints.length - 2) {
+                newCtrlPoints[i] = [
+                    controlPoints[i][2],
+                    controlPoints[i][3],
+                    controlPoints[i + 1][2],
+                    controlPoints[i + 1][3],
+                ];
+            }
+        }
 
-        //     // path.moveTo(x1, y1);
-        //     path.bezierCurveTo(cpx1, cpy1 ,cpx2, cpy2, x1, y1);
-        //     context.stroke(path);
-        // }
+        // console.log(newCtrlPoints);
 
-        path.moveTo(points[0][0], points[0][1]);
 
-        points.forEach(([x1, y1], index) => {
-            const [x2, y2] = points[index + 1] || [x1, y1];
-            const [xMid, yMid] = midpoint([x1, y1], [x2, y2]);
-            const cpx1 = (x1 + xMid) / 2;
-            const cpx2 = (x2 + xMid) / 2;
 
-            path.quadraticCurveTo(cpx1, y1, xMid, yMid);
-            path.quadraticCurveTo(cpx2, y2, x2, y2);
-        });
+        path.moveTo(pnts[0][0], pnts[0][1]);
+
+        for (let index = 1; index <= points.length - 1; index++) {
+            // console.log('helo');
+            // const [x0, y0] = pnts[index - 1];
+            // const [x1, y1] = pnts[index];
+            const [x2, y2] = pnts[index + 1];
+
+            // const [cpx1, cpy1, cpx2, cpy2] = getControlPoint([x0, y0], [x1, y1], [x2, y2]);
+            const [cpx1, cpy1, cpx2, cpy2] = newCtrlPoints[index - 1];
+            // path.moveTo(x1, y1);
+            path.bezierCurveTo(cpx1, cpy1 ,cpx2, cpy2, x2, y2);
+            context.stroke(path);
+        }
+
+        // path.moveTo(points[0][0], points[0][1]);
+
+        // points.forEach(([x1, y1], index) => {
+        //     const [x2, y2] = points[index + 1] || [x1, y1];
+        //     const [xMid, yMid] = midpoint([x1, y1], [x2, y2]);
+        //     const cpx1 = (x1 + xMid) / 2;
+        //     const cpx2 = (x2 + xMid) / 2;
+
+        //     path.quadraticCurveTo(cpx1, y1, xMid, yMid);
+        //     path.quadraticCurveTo(cpx2, y2, x2, y2);
+        // });
     },
 });
