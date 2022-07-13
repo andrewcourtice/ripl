67036634b21ef4c3b6eefd283d6df8191393e483
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2022-07-13 17:15:55 +1000

    feat(website): added more demos

---
+++ apps/website/src/components/examples/interactions.svelte
@@ -0,0 +1,99 @@
+<Example>
+    <div slot="sidebar">
+        <button on:click={transitionCircles}>Transition</button>
+    </div>
+</Example>
+
+<script lang="ts">
+import Example from './base/example.svelte';
+
+import {
+    onMount
+} from 'svelte';
+
+import {
+    circle,
+    continuous,
+    easeOutQuint,
+    group,
+    line,
+    renderer,
+    scene,
+} from '@ripl/core';
+
+let transitionCircles = () => {};
+
+onMount(() => {
+    let x = 0;
+    let y = 0;
+
+    const mainScene = scene('.example__canvas');
+    const mainRenderer = renderer(mainScene, {
+        autoStart: false
+    });
+
+    const rScale = continuous([0, 1], [5, 15]);
+    const xScale = continuous([0, 1], [10, mainScene.canvas.width - 10]);
+    const yScale = continuous([0, 1], [10, mainScene.canvas.height - 10]);
+
+    const circles = Array.from({ length: 1000 }, (_, index) => circle({
+        radius: [rScale(Math.random()), rScale(Math.random())],
+        x: [xScale(Math.random()), xScale(Math.random())],
+        y: [yScale(Math.random()), yScale(Math.random())],
+    }));
+    
+    const circleGroup = group({
+        fillStyle: '#333333',
+    });
+
+    const crosshairHLine = line({
+        points: () => [
+            [x, 0],
+            [x, mainScene.canvas.height]
+        ]
+    });
+
+    const crosshairVLine = line({
+        points: () => [
+            [0, y],
+            [mainScene.canvas.width, y]
+        ]
+    });
+
+    const crosshairGroup = group({
+        strokeStyle: '#CCCCCC'
+    });
+    
+    circleGroup.add(circles);
+    crosshairGroup.add([
+        crosshairHLine,
+        crosshairVLine,
+    ]);
+
+    mainScene.add([
+        circleGroup,
+        crosshairGroup
+    ]);
+
+    mainScene.on('scenemousemove', (sx, sy) => {
+        x = sx;
+        y = sy;
+    });
+    
+    transitionCircles = () => {
+        circles.forEach(crc => crc.to({
+            radius: rScale(Math.random()),
+            x: xScale(Math.random()),
+            y: yScale(Math.random()),
+        }));
+
+        mainRenderer.transition(circles, {
+            duration: 3000,
+            ease: easeOutQuint,
+            delay: index => index * (1000 / circles.length)
+        });
+    }
+
+    mainRenderer.start();
+});
+</script>
---
+++ apps/website/src/components/examples/transitions.svelte
@@ -0,0 +1,66 @@
+<Example>
+    <div slot="sidebar">
+        <button on:click={transitionCircles}>Transition</button>
+    </div>
+</Example>
+
+<script lang="ts">
+import Example from './base/example.svelte';
+
+import {
+    onMount
+} from 'svelte';
+
+import {
+    getContext,
+    circle,
+    group,
+    continuous,
+    transition,
+    easeOutQuint,
+} from '@ripl/core';
+
+let transitionCircles = () => {};
+
+onMount(() => {
+    const {
+        canvas,
+        context,
+        clear
+    } = getContext('.example__canvas');
+
+    const rScale = continuous([0, 1], [5, 15]);
+    const xScale = continuous([0, 1], [10, canvas.width - 10]);
+    const yScale = continuous([0, 1], [10, canvas.height - 10]);
+
+    const circles = Array.from({ length: 1000 }, (_, index) => circle({
+        radius: [rScale(Math.random()), rScale(Math.random())],
+        x: [xScale(Math.random()), xScale(Math.random())],
+        y: [yScale(Math.random()), yScale(Math.random())],
+    }));
+    
+    const grp = group({
+        fillStyle: '#000000',
+    });
+    
+    grp.add(circles);
+    
+    transitionCircles = () => {
+        circles.forEach(crc => crc.to({
+            radius: rScale(Math.random()),
+            x: xScale(Math.random()),
+            y: yScale(Math.random()),
+        }));
+
+        transition(time => {
+            clear();
+            grp.render(context, time);
+        }, {
+            duration: 3000,
+            ease: easeOutQuint
+        })
+    };
+    
+    grp.render(context);
+});
+</script>
