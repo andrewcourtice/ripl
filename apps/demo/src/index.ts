600a5104aa77a5ec4cfbb0a339ea4a46b074008f
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2023-02-21 11:42:59 +1000

    chore: removed old demo app

--- apps/demo/src/index.ts
+++
@@ -1,8 +0,0 @@
-import 'flex-layout-attribute';
-import './assets/styles/index.scss';
-
-import App from './app.svelte';
-
-export default new App({
-    target: document.getElementById('app'),
-});
