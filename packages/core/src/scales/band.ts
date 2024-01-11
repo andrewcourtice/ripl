001aa6b86eb137245df78b2b830458ce0cbd788d
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2024-01-11 15:57:30 +1000

    feat: added band scale

---
+++ packages/core/src/scales/band.ts
@@ -0,0 +1,59 @@
+import {
+    bindScale,
+} from './_base';
+
+import {
+    max,
+} from '../math';
+
+import type {
+    Scale,
+} from './types';
+
+export interface BandScale<TDomain> extends Scale<TDomain> {
+    bandwidth: number;
+    step: number;
+}
+
+export type BandScaleOptions = {
+    innerPadding?: number;
+    outerPadding?: number;
+    alignment?: number;
+    round?: boolean;
+}
+
+export function scaleBand<TDomain>(
+    domain: TDomain[],
+    range: number[],
+    options?: BandScaleOptions
+): BandScale<TDomain> {
+    const {
+        innerPadding = 0,
+        outerPadding = 0,
+        alignment = 0.5,
+        round,
+    } = options || {};
+
+    const [
+        rangeMin,
+        rangeMax,
+    ] = range;
+
+    const rangeLength = rangeMax - rangeMin;
+    const domainLength = domain.length - innerPadding;
+    const step = rangeLength / max(1, domainLength + outerPadding * 2);
+    const bandwidth = step * (1 - innerPadding);
+    const adjustedMin = rangeMin + (rangeLength - step * domainLength) * alignment;
+
+    const scale = bindScale({
+        domain,
+        range,
+        convert: value => adjustedMin + (domain.indexOf(value) * step),
+        invert: value => domain[Math.floor(value / step)],
+    }) as BandScale<TDomain>;
+
+    scale.bandwidth = bandwidth;
+    scale.step = step;
+
+    return scale;
+}
